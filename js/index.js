9756921733951f4ce2d0a9a9ccb6712280bbc81d
ebisLab <ebaiden@techie.com>
2019-06-18 18:02:56 -0400

    popup eventlistener

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // Your code goes here
+//mouseenter
+
 let linx = document.querySelectorAll('.nav-link');
 
 linx.forEach(anchor => {
@@ -8,3 +10,11 @@ setTimeout(function(){e.target.style.color='';
 }, 500)
 }, false);
 });
+
+
+//pop up event listener
+
+let firstPop = document.querySelector('.btn')
+
+firstPop.addEventListener('click', function(){
+    alert ('Imma let you finish, but Beyonce had one of the best albums ever. Sorry, not sorry')})
