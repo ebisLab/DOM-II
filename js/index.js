82ecb19d9348c8a799dc3999bfee585438f89d5c
ebisLab <ebaiden@techie.com>
2019-06-18 17:21:54 -0400

    nav red hover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+let linx = document.querySelectorAll('.nav-link');
+
+linx.forEach(anchor => {
+    anchor.addEventListener('mouseenter', function(e){
+e.target.style.color= 'red';
+setTimeout(function(){e.target.style.color='';
+}, 500)
+}, false);
+});
